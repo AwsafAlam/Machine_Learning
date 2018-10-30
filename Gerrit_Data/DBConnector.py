92fe6717721cce6f86cc23a1c71c37ef85b72324
AwsafAlam <1505114.maaa@ugrad.cse.buet.ac.bd>
2018-10-31 02:14:17 +0600

    Added Connecting to DB , dictionary not working

---
+++ Gerrit_Data/DBConnector.py
@@ -0,0 +1,4 @@
+
+class DBConnector(object):
+
+    def __init__(self, host, user, password, db_name):
--- Gerrit_Data/extract_json.py
+++ Gerrit_Data/extract_json.py
@@ -8,7 +8,10 @@ connection = pymysql.connect(host='localhost',
                              db='gerrit_test',
                              charset='utf8mb4',
                              cursorclass=pymysql.cursors.DictCursor)
-
+def search(req_id):
+    for p in request_detail:
+        if p['request_id'] == req_id:
+            return p
 
 try:
     # with connection.cursor() as cursor:
@@ -20,25 +23,102 @@ try:
     # your changes.
     # connection.commit()
 
+    
+    # with connection.cursor() as cursor:
+    #     # Read a single record
+    #     sql = "SELECT * FROM inline_comments"
+    #     cursor.execute(sql)
+    #     result = cursor.fetchall()
+    #     inline_comments = json.dumps(result , default=str)
+        
+    #     # print(y)
+
+    # with connection.cursor() as cursor:
+    #     # Read a single record
+    #     sql = "SELECT * FROM patches"
+    #     cursor.execute(sql)
+    #     result = cursor.fetchall()
+    #     patches = json.dumps(result , default=str)
+
+    #     # print(y)
+
+    # with connection.cursor() as cursor:
+    #     # Read a single record
+    #     sql = "SELECT * FROM patch_details"
+    #     cursor.execute(sql)
+    #     result = cursor.fetchall()
+    #     patch_details = json.dumps(result , default=str)
+
+    #     # print(y)
+
+    # with connection.cursor() as cursor:
+    #     # Read a single record
+    #     sql = "SELECT * FROM people"
+    #     cursor.execute(sql)
+    #     result = cursor.fetchall()
+    #     people = json.dumps(result , default=str)
+
+    
     with connection.cursor() as cursor:
         # Read a single record
-        sql = "SELECT * FROM people"
+        sql = "SELECT * FROM requests"
         cursor.execute(sql)
         result = cursor.fetchall()
-        y = json.dumps(result)
+        tmp = {}
+        for row in result:
+            tmp = row
+            tmp["last_updated_on"] = str(row["last_updated_on"])
+            tmp["created"] = str(row["created"])
 
-        print(y)
+        requests = json.dumps(tmp)
 
-        # for row in result:
+        # print(y)
 
-        #     print(row["gerrit_id"])
 
-        #     print(row["full_name"])   
+    with connection.cursor() as cursor:
+        # Read a single record
+        sql = "SELECT * FROM request_detail"
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        for row in result:
+            row["created"] = str(row["created"])
+            row["updated"] = str(row["updated"])
 
-        #     print(row["preferred_email"])   
+        
+        request_detail = json.dumps(result)
 
-        #     print(row["username"])   
+        # print(y)
 
-        # print(result)
+    # with connection.cursor() as cursor:
+    #     # Read a single record
+    #     sql = "SELECT * FROM reviews"
+    #     cursor.execute(sql)
+    #     result = cursor.fetchall()
+    #     reviews = json.dumps(result , default=str)
+
+        # print(y)
+
+    # with connection.cursor() as cursor:
+    #     # Read a single record
+    #     sql = "SELECT * FROM review_comments"
+    #     cursor.execute(sql)
+    #     result = cursor.fetchall()
+    #     review_comments = json.dumps(result , default=str)
+
+    # print(requests)
+    # print(people)
+
+    finalJSON = {}
+
+    for row in requests:
+        finalJSON["request_id"] = {
+            "change_id" : search(row["request_id"])
+        } 
+
+    y = json.dumps(finalJSON)
+    print(y)
+
 finally:
     connection.close()
+
+
