062199819e61d036984e29b5291a3d8f1807222f
AwsafAlam <1505114.maaa@ugrad.cse.buet.ac.bd>
2019-04-26 15:04:07 +0600

    Added preprocessing templates

--- MyCodes/Preprocessing_Template.py
+++ MyCodes/Preprocessing_Template.py
@@ -9,6 +9,7 @@ Y = dataset.iloc[:, 3].values # Dependent
 
 # Splitting into Training set and test dataset
 from sklearn.model_selection import train_test_split
+
 X_train, X_test , Y_train , Y_test = train_test_split(X,Y , test_size = 0.2 , random_state = 0)
 
 # Feature Scaling
@@ -20,4 +21,4 @@ X_train = sc_X.fit_transform(X_train) # Fit the StandardScaler object to the tra
 X_test = sc_X.transform(X_test) # Same object, so no need to Fit
 '''
 
-print(X_train);
+print(X_train)
